cb2497ab3301921929bfb0658cb6763a5ae138c9
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-05-12 15:09:14 -0400

    data from animate funct

--- plot009.py
+++ plot009.py
@@ -2,20 +2,27 @@ import random
 from itertools import count
 import pandas as pd
 from matplotlib import pyplot as plt 
+from matplotlib.animation import FuncAnimation
 
 plt.style.use('fivethirtyeight')
 
-x_vals = [0, 1, 2, 3, 4, 5]
-y_vals = [0, 1, 3, 2, 3, 5]
+x_vals = []
+y_vals = []
 
-plt.plot(x_vals, y_vals)
+index = count()
+
+def animate(i):
+    x_vals.append(next(index))
+    y_vals.append(random.randint(0, 5))
+    plt.cla()
+    plt.plot(x_vals, y_vals)
 
 
-# index = count()
-
-# def animate(i):
-#     x_vals.append(next(index))
-#     y_vals.append(random.randint(0, 5))
+ani = FuncAnimation(plt.gcf(), animate, interval=1000)
 
 plt.tight_layout()
 plt.show()
+
+data = pd.read_csv('data.csv')
+x = data['x_value']
+y1 = data['total_1']
