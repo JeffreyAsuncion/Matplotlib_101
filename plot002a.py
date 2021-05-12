a2a23b3b4f0921b838f245baa42b4c3fa9ea2f1a
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-05-11 20:12:17 -0400

    plot bar and pies charts

---
+++ plot002a.py
@@ -0,0 +1,38 @@
+import csv
+import numpy as np
+from collections import Counter
+from matplotlib import pyplot as plt
+
+
+plt.style.use("fivethirtyeight")
+
+
+with open('data.csv') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    language_counter = Counter()
+
+    for row in csv_reader:
+        language_counter.update(row['LanguagesWorkedWith'].split(';'))
+
+languages = []
+popularity = []
+
+for item in language_counter.most_common(15):
+    languages.append(item[0])
+    popularity.append(item[1])
+
+
+print(language_counter.most_common(15))
+print(languages.reverse())
+print(popularity.reverse())
+
+plt.barh(languages, popularity)
+
+plt.title("Most Popular Languages")
+plt.xlabel("Number of People Who Use")
+# plt.ylabel("Programming Languages")
+
+plt.tight_layout()
+
+plt.show()
---
+++ plot002b.py
@@ -0,0 +1,41 @@
+import csv
+import numpy as np
+import pandas as pd
+from collections import Counter
+from matplotlib import pyplot as plt
+
+
+plt.style.use("fivethirtyeight")
+
+data = pd.read_csv('data.csv')
+ids = data['Responder_id']
+lang_responses = data['LanguagesWorkedWith']
+
+
+
+language_counter = Counter()
+
+for response in lang_responses:
+    language_counter.update(response.split(';'))
+
+languages = []
+popularity = []
+
+for item in language_counter.most_common(15):
+    languages.append(item[0])
+    popularity.append(item[1])
+
+
+print(language_counter.most_common(15))
+print(languages.reverse())
+print(popularity.reverse())
+
+plt.barh(languages, popularity)
+
+plt.title("Most Popular Languages")
+plt.xlabel("Number of People Who Use")
+# plt.ylabel("Programming Languages")
+
+plt.tight_layout()
+
+plt.show()
